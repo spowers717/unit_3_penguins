934cc5b3a9c43b6a852e394acae7e06b77abe69a
spowers717 <122506167+spowers717@users.noreply.github.com>
2023-03-02 11:23:24 -0500

    updates

--- class_notes_2023_02_28.R
+++ class_notes_2023_02_28.R
@@ -101,9 +101,115 @@ ggplot() +
   geom_ribbon(data=newdata_predict_lm_3, aes(ymin=lwr, ymax=upr, x=bill_length_mm, fill = species), alpha=0.5) +
   geom_line(data = newdata_predict_lm_3, aes(y=fit, x=bill_length_mm, color=species))
 
+# interaction term
+
+lm_4 = lm(bill_depth_mm ~bill_length_mm + species + bill_length_mm:species, 
+          data= penguins_lm_3)
+lm_4 = lm(bill_depth_mm ~bill_length_mm:species, # bad
+          data= penguins_lm_3)
+# this code is saying the same thing as the first model
+lm_4 = lm(bill_depth_mm ~bill_length_mm * species, 
+          data= penguins_lm_3)
+summary(lm_4)
+# added model complexity does not benefit you as far as improving the predictions adjusted r square is basically the same
+
+AIC(lm_3, lm_4)
+# AIC smaller is better
+# AIC 2 units smaller to be considered a better model
+# lm_3 is 3 AIC units less than lm_4 
+step(lm_4)
+# removes variables and asks if the model is better. Keeps going to find the best fit model with the lowest complexity. 
+best_model = step(lm_4)
+summary(best_model)
+
+#plot with interaction
+
+lm_4_predict = lm_4 %>% #recall this is our bad model
+  broom::augment(interval = "confidence")
+head(lm_4_predict)
+
+ggplot(data= lm_4_predict) +
+  geom_point(aes(x= bill_length_mm, y= bill_depth_mm, color = species)) +
+  geom_line(aes(y=.fitted, x= bill_length_mm, color = species)) +
+  geom_ribbon(aes(ymin=.lower, ymax=.upper, x=bill_length_mm, fill = species), alpha=0.5)
+
+# depth ~ bill_length + flipper_length
+
+library(car) #VIF() variance inflation factor to make sure we don't have multi-colinearity 
+
+gentoo = penguins %>%
+  filter(species=="Gentoo")
 
 
+lm_gentoo_1 = lm(bill_depth_mm ~ bill_length_mm, data=gentoo)
+lm_gentoo_2 = lm(bill_depth_mm ~ bill_length_mm + flipper_length_mm, data=gentoo)
+lm_gentoo_3 = lm(bill_depth_mm ~ bill_length_mm + flipper_length_mm + body_mass_g, data=gentoo)
 
+AIC(lm_gentoo_1, lm_gentoo_2, lm_gentoo_3)
+# lm_gentoo_3 has the best model
+step(lm_gentoo_3)
+# says to keep the model
+vif(lm_gentoo_3)
+
+head(penguins_lm_3)
+
+newdata = gentoo %>%
+  select(bill_length_mm) %>%
+  mutate(flipper_length_mm = median(gentoo$flipper_length_mm, na.rm = TRUE),
+         body_mass_g = median(gentoo$body_mass_g, na.rm = TRUE))
+
+head(newdata)
+lm_gentoo_3_predict = lm_gentoo_3 %>%
+  broom::augment(newdata=newdata, interval = "confidence")
+head(lm_gentoo_3_predict)
+
+ggplot(data= lm_gentoo_3_predict) +
+  geom_point(aes(x= bill_length_mm, y=bill_depth_mm), data = gentoo) +
+  geom_line(aes(y= .fitted, x = bill_length_mm)) +
+  geom_ribbon(aes(ymin=.lower, ymax=.upper, x=bill_length_mm), alpha=0.5) +
+  annotate("text",  x= 57, y=13.5, 
+          label= paste0("flipper length = ", 
+          median(gentoo$flipper_length_mm, na.rm = TRUE), " mm")) +
+  annotate("text",  x= 57, y=14, 
+          label= paste0("body mass = ", 
+          median(gentoo$body_mass_g, na.rm = TRUE), " g"))
+
+
+# 5.3
+
+newdata = gentoo %>%
+  select(flipper_length_mm) %>%
+  mutate(bill_length_mm = median(gentoo$bill_length_mm, na.rm = TRUE),
+         body_mass_g = median(gentoo$body_mass_g, na.rm = TRUE))
+head(newdata)
+
+head(newdata)
+lm_gentoo_3_predict = lm_gentoo_3 %>%
+  broom::augment(newdata=newdata, interval = "confidence")
+head(lm_gentoo_3_predict)
+
+ggplot(data= lm_gentoo_3_predict) +
+  geom_point(aes(x= flipper_length_mm, y=bill_depth_mm), data = gentoo) +
+  geom_line(aes(y= .fitted, x = flipper_length_mm )) +
+  geom_ribbon(aes(ymin=.lower, ymax=.upper, x=flipper_length_mm), alpha=0.5) +
+  annotate("text",  x= 225, y=13.5, 
+           label= paste0("bill length = ", 
+                         median(gentoo$bill_length_mm, na.rm = TRUE), " mm")) +
+  annotate("text",  x= 225, y=13.7, 
+           label= paste0("body mass = ", 
+                         median(gentoo$body_mass_g, na.rm = TRUE), " g")) +
+  theme_classic()
+
+# ANOVA
+penguin_lm = lm(body_mass_g~species + sex, data = penguins)
+anova(penguin_lm)
+
+penguin_anova = aov(body_mass_g ~species + sex, data = penguins)
+summary(penguin_anova)
+
+TukeyHSD(penguin_anova)
+
+  
 
 
 
